2f6c3b63453546729399ae6590119d8b8cc0f1e7
Star <109724704+Star369666@users.noreply.github.com>
2023-03-14 22:10:50 +0800

    Add files via upload

---
+++ HW1/halfadd/halfadd.vhd
@@ -0,0 +1,17 @@
+--use Quartus II library--
+library ieee;
+use ieee.std_logic_1164.all;
+
+--half adder declaration--
+entity halfadd is 
+    port(
+        X, Y: in std_logic;
+        Sum, Carry: out std_logic
+    );
+end halfadd;
+
+architecture halfadd_part of halfadd is 
+begin
+    Sum <= X xor Y;
+    Carry <= X and Y;
+end;
